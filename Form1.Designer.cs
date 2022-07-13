ef0f58bf28525830d0f9bcdd4ae2d2b5b580d1ec
unknown <andreev.tihon@gmail.com>
2022-07-13 08:13:32 +0300

    1 & 2 labs

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace pr_teory___lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lambda";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "new experiment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(310, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(106, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(310, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "experiment series";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(89, 86);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(97, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "36";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Numb";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Rand";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(274, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(9, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "j";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(441, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(335, 111);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(23, 133);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(393, 97);
+            this.dataGridView2.TabIndex = 14;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(23, 250);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(393, 211);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 517);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace pr_teory___lab1
+{
+    public partial class Form1 : Form
+    {
+        double Prob = 0.0, Delta = 0.0;
+        double L, T;
+
+        double maxDisp;
+
+        int n = 0;
+        int[] Xj;
+        int[] val;
+        double[] F;
+        double[] expF;
+
+        public Form1()
+        {
+            maxDisp = 0.0;
+            InitializeComponent();
+            n = Convert.ToInt32(textBox5.Text);
+            Xj = new int[32000];
+            val = new int[n];
+            expF = new double[n];
+            F = new double[n];
+            for (int i = 0; i < 32000; i++) Xj[i] = -1;
+            for (int i = 0; i < n; i++) val[i] = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private double Probability(int k)
+        {
+            double P = Math.Pow(L * T, k) * Math.Exp(-L * T);
+            for (int i = 1; i <= k; i += 1) P /= (double)i;
+            return P;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 32000; i++)
+                Xj[i] = -1;
+
+
+            dataGridView1.Columns.Clear();
+            n = Convert.ToInt32(textBox5.Text);
+
+            val = new int[n];
+            F = new double[n];
+            expF = new double[n];
+
+            double x;
+
+
+            L = Convert.ToDouble(textBox1.Text);
+            T = Convert.ToDouble(textBox2.Text);
+            double Lambda = L * T;
+
+            Random rnd = new Random();
+            for (int j = 0; j < n; j++)
+            {
+                x = (double)rnd.Next(0, 32000) / (32000.0);
+
+                Prob = Math.Exp(-Lambda);
+                Delta = Prob;
+
+                int i = 0;
+                while (Delta < x)
+                {
+                    i++;
+                    Prob /= (double)i;
+                    Prob *= Lambda;
+                    Delta += Prob;
+                }
+                expF[j] = Delta;
+                if (Xj[i] == -1) Xj[i]++;
+                Xj[i]++;
+            }
+
+            int upperNull = 0;
+            for (int i = 0; i < 32000; i++)
+                if (Xj[i] >= 0)
+                {
+                    upperNull++;
+                }
+
+
+
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = upperNull + 1;
+            int m = 0;
+            for (int j = 0; j < 32000; j++)
+            {
+                if (Xj[j] >= 0)
+                {
+                    dataGridView1.Columns[m].Name = Convert.ToString(j);
+                    dataGridView1.Rows[0].Cells[m].Value = Convert.ToString(Xj[j]);
+                    dataGridView1.Rows[1].Cells[m].Value = Convert.ToString(Probability(j));
+                    dataGridView1.Rows[2].Cells[m].Value = Convert.ToString((double)Xj[j] / (double)n);
+
+                    if (Math.Abs(Probability(j) - (double)Xj[j] / (double)n) > maxDisp) maxDisp = Math.Abs(Probability(j) - (double)Xj[j] / (double)n);
+
+                    m++;
+                }
+            }
+
+            Table2();
+        }
+
+        private void Table2()
+        {
+            double En, midX = 0.0, Dn = 0.0, S2 = 0.0, Me = 0.0, midR = 0.0;
+            // Математическое ожидание
+            En = L * T;
+            // Дисперсия
+            Dn = L * T;
+
+            int j = 0;
+
+            for (int i = 0; i < 32000; i++)
+            {
+                if (Xj[i] >= 0)
+                {
+                    for (int k = 0; k < Xj[i]; k++)
+                        val[j + k] = i;
+                    j += Xj[i];
+                }
+            }
+
+            // Размах выборки
+            midR = val[j - 1] - val[0];
+
+            // Выборочное среднее
+            for (int i = 0; i < n; i++)
+            {
+                midX += val[i];
+            }
+
+            midX = (double)midX / (double)(n);
+
+            // Выборочная дисперсия
+            for (int i = 0; i < n; i++)
+            {
+                S2 += (val[i] - midX) * (val[i] - midX);
+            }
+            S2 = (double)S2 / (double)(n);
+
+            // Медиана
+
+            if (n % 2 == 0)
+                Me = (double)(val[n / 2] + val[n / 2 + 1]) / 2;
+            else
+                Me = (double)val[n / 2] / 2;
+
+            dataGridView2.RowCount = 2;
+            dataGridView2.ColumnCount = 9;
+
+            dataGridView2.Rows[0].Cells[0].Value = "En";                
+            dataGridView2.Rows[1].Cells[0].Value = Convert.ToString(En);
+            dataGridView2.Rows[0].Cells[1].Value = "middle X";          
+            dataGridView2.Rows[1].Cells[1].Value = Convert.ToString(midX);
+            dataGridView2.Rows[0].Cells[2].Value = "|En - middle X|";   
+            dataGridView2.Rows[1].Cells[2].Value = Convert.ToString(Math.Abs(En - midX));
+            dataGridView2.Rows[0].Cells[3].Value = "Dn";                
+            dataGridView2.Rows[1].Cells[3].Value = Convert.ToString(Dn);
+            dataGridView2.Rows[0].Cells[4].Value = "S^2";               
+            dataGridView2.Rows[1].Cells[4].Value = Convert.ToString(S2);
+            dataGridView2.Rows[0].Cells[5].Value = "|Dn - S^2|";        
+            dataGridView2.Rows[1].Cells[5].Value = Convert.ToString(Math.Abs(Dn - S2));
+            dataGridView2.Rows[0].Cells[6].Value = "Me";                
+            dataGridView2.Rows[1].Cells[6].Value = Convert.ToString(Me);
+            dataGridView2.Rows[0].Cells[7].Value = "R";                 
+            dataGridView2.Rows[1].Cells[7].Value = Convert.ToString(midR);
+
+            dataGridView2.Rows[0].Cells[8].Value = "max|Pj - nj/n|";
+            dataGridView2.Rows[1].Cells[8].Value = Convert.ToString(maxDisp);
+
+            drawChart();
+        }
+
+        double Fn(int x)
+        {               
+            double F = 0.0;
+            int i = 0;
+            while (val[i] < x)
+            { 
+                F += Probability(val[i]);
+                i++;
+                if (i == n) break;
+            }
+
+            F /= (double)n;
+            return F;
+        }
+
+        private void drawChart()
+        {
+            Array.Sort(val);
+            double FuckYou = Probability(val[0]);
+            F[0] = Probability(val[0]);
+            for(int i = 1; i < n; i ++)
+            {
+                if (val[i] != val[i - 1])
+                    FuckYou += Probability(val[i]);
+                F[i] = FuckYou;
+            }
+            Array.Sort(expF);
+
+            double[] _F = new double[n + 2];
+            double[] _eF = new double[n + 2];
+            int[] _val = new int[n + 2];
+            _F[0] = F[0];
+            _eF[0] = expF[0];
+            _val[0] = val[0] - 1;
+
+            for (int i = 1; i < n + 1; i ++)
+            {
+                _F[i] = F[i - 1];
+                _eF[i] = expF[i - 1];
+                _val[i] = val[i - 1];
+            }
+
+            _F[n + 1] = 1;
+            _eF[n + 1] = 1;
+            _val[n + 1] = val[n - 1] + 1;
+
+            // добавим серию для теоритической Fn
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            chart1.ChartAreas.Add(new ChartArea("first"));
+            chart1.Series.Add(new Series("Fn"));
+            chart1.Series["Fn"].ChartArea = "first";
+            chart1.Series["Fn"].ChartType = SeriesChartType.StepLine;
+
+            chart1.Series["Fn"].Points.DataBindXY(_val, _F);
+            chart1.ChartAreas[0].AxisX.Interval = 10;
+
+            chart1.Series.Add(new Series("expFn"));
+            chart1.Series["expFn"].ChartArea = "first";
+            chart1.Series["expFn"].ChartType = SeriesChartType.StepLine;
+            chart1.Series["expFn"].Points.DataBindXY(_val, _eF);
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            L = Convert.ToDouble(textBox1.Text);
+            T = Convert.ToDouble(textBox2.Text);
+
+            Random rnd = new Random();
+            double x = (double)rnd.Next(0, 32000) / 32000.0;
+
+            double LT = L * T;
+            double k = 1.0;
+
+            Prob = Math.Exp(-LT);
+            Delta = Prob;
+
+
+            int i = 0;
+            while (Delta < x)
+            {
+                i++;
+                k = LT / (double)i;
+                Prob *= k;
+                Delta += Prob;
+            }
+
+            textBox3.Text = Convert.ToString(x);
+            textBox4.Text = Convert.ToString(i);
+        }
+    }
+}
